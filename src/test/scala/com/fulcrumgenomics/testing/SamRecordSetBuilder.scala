76bec1f9dfdf98a30dc8ab03634627a3b3d44436
Nils Homer <nilshomer@gmail.com>
2016-06-27 23:42:09 -0400

    WIP: FilterBam tests pass again.

--- src/test/scala/com/fulcrumgenomics/testing/SamRecordSetBuilder.scala
+++ src/test/scala/com/fulcrumgenomics/testing/SamRecordSetBuilder.scala
@@ -51,7 +51,7 @@ class SamRecordSetBuilder(val readLength: Int=100,
                           val sortOrder: SortOrder = SortOrder.unsorted,
                           val readGroupId: Option[String] = None
                          ) extends Iterable[SAMRecord] {
-  private val builder = new SAMRecordSetBuilder(sortOrder != SortOrder.unsorted, sortOrder, readGroupId.isDefined)
+  private val builder = new SAMRecordSetBuilder(sortOrder != SortOrder.unsorted, sortOrder)
   readGroupId.foreach { id =>
     val readGroup = new SAMReadGroupRecord(id)
     readGroup.setSample("Sample")
