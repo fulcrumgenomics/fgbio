414f07c92d15c68b13c7a2bcc44f8748d098c892
tfenne <tfenne@tfenne.com>
2017-05-08 09:04:33 -0700

    Cleaning up a bit.

--- src/main/scala/com/fulcrumgenomics/personal/tfenne/SortBam.scala
+++ src/main/scala/com/fulcrumgenomics/personal/tfenne/SortBam.scala
@@ -23,20 +23,20 @@
  */
 
 package com.fulcrumgenomics.personal.tfenne
-
-import java.io.{ByteArrayOutputStream, InputStream}
+import java.io.{ByteArrayOutputStream, Closeable, InputStream}
 import java.nio.file.{Files, Path}
 import java.util
 
 import com.fulcrumgenomics.FgBioDef._
 import com.fulcrumgenomics.cmdline.{ClpGroups, FgBioTool}
-import com.fulcrumgenomics.personal.tfenne.Sorter.{MergingIterator, SortEntry}
+import com.fulcrumgenomics.personal.tfenne.DiskBackedSorter.SortEntry
 import com.fulcrumgenomics.util.{Io, ProgressLogger}
 import dagr.commons.util.LazyLogging
 import dagr.sopt.{arg, clp}
 import htsjdk.samtools.SAMFileHeader.SortOrder
 import htsjdk.samtools.util.{SortingCollection, TempStreamFactory}
 import htsjdk.samtools.{BAMRecordCodec, SAMFileWriterFactory, SAMRecord, SamReaderFactory}
+import SortingCollection.Codec
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -56,7 +56,7 @@ class SortBam
     val in     = SamReaderFactory.make().open(input)
     val header = in.getFileHeader.clone()
     header.setSortOrder(SortOrder.coordinate)
-    val sorter = new Sorter(maxRecordsInRam, new BAMRecordCodec(header), BamCoordinateKey.apply)
+    val sorter = new DiskBackedSorter(maxRecordsInRam, new BAMRecordCodec(header), BamCoordinateKey.apply)
     val out    = new SAMFileWriterFactory().setCreateIndex(true).makeWriter(header, true, output.toFile, null)
 
     val sortProgress  = new ProgressLogger(logger, verb="sorted", unit=5e6.toInt)
@@ -96,53 +96,89 @@ case class BamCoordinateKey(refIndex: Int, pos: Int, flags: Int) extends Compara
   }
 }
 
-object Sorter {
+/**
+  * Companion object for [[DiskBackedSorter]] that contains various types used.
+  */
+object DiskBackedSorter {
+  /**
+    * A specialized tuple that contains a key object that is [[Comparable]] and a serialized version
+    * of the object being sorted in an [[Array]].  The entry itself is comparable but simply
+    * delegates comparison to the key object.
+    *
+    * @param key a comparable key that contains necessary information from the object being sorted
+    * @param bytes a serialized form of the object read to be written to disk
+    * @tparam B the key type
+    */
   case class SortEntry[B <: Comparable[B]](key: B, bytes: Array[Byte]) extends Comparable[SortEntry[B]] {
     override def compareTo(that: SortEntry[B]): Int = this.key.compareTo(that.key)
   }
+}
 
-  case class MergeEntry[A,B <: Comparable[B]](rec: A, key: B) extends Comparable[MergeEntry[A,B]] {
-    override def compareTo(that: MergeEntry[A, B]): Int = this.key.compareTo(that.key)
-  }
+/** An implementation of a disk-backed sorting system. */
+class DiskBackedSorter[A,B <: Comparable[B]](val maxRecordsInRam: Int,
+                                             private val codec: Codec[A],
+                                             private val keyfunc: A => B) extends Iterable[A] {
+  require(maxRecordsInRam > 1, "Max records in RAM must be at least 2, and probably much higher!")
+  private val stash = new Array[SortEntry[B]](maxRecordsInRam)
+  private val files = new ArrayBuffer[Path]()
+  private var recordsInMemory: Int = 0
+  private val byteStream = new ByteArrayOutputStream(128 * 1024) // TODO: how to size this?
+  private val _tmpStreamFactory = new TempStreamFactory
+  codec.setOutputStream(byteStream)
 
-  /** An iterator that consumes from a single stream for a single tmp sort file. */
-  private class SingleStreamIterator[A,B <: Comparable[B]](stream: InputStream,
-                                                           codec: SortingCollection.Codec[A],
-                                                           keyfunc: A => B
-                                                          ) extends Iterator[MergeEntry[A,B]] with Comparable[SingleStreamIterator[A,B]] {
+  /**
+    * An iterator that consumes data from a single tmp file of sorted data and produces
+    * an iterator of sorted records.
+    */
+  private class SortedIterator(stream: InputStream, codec: Codec[A]) extends Iterator[A] with Comparable[SortedIterator] with Closeable  {
+    private var nextValue : A = _
+    private var nextKey   : B = _
     codec.setInputStream(stream)
-    private var nextEntry = decodeNext
+    advance()
 
-    private def decodeNext: MergeEntry[A,B] = {
+    private def advance(): Unit = {
       val rec = this.codec.decode()
-      if (rec == null) null else MergeEntry(rec, keyfunc(rec))
+      if (rec == null) {
+        nextValue = null.asInstanceOf[A]
+        nextKey   = null.asInstanceOf[B]
+      }
+      else {
+        nextValue = rec
+        nextKey   = keyfunc(rec)
+      }
     }
 
-    override def hasNext: Boolean = nextEntry != null
+    override def hasNext: Boolean = nextValue != null
 
-    override def next(): MergeEntry[A, B] = {
-      if (nextEntry == null) throw new NoSuchElementException("next called on empty FileIterator.")
-      val retval = this.nextEntry
-      this.nextEntry = decodeNext
+    override def next(): A = {
+      if (!hasNext) throw new NoSuchElementException("next called on empty Iterator")
+      val retval = this.nextValue
+      advance()
       retval
     }
 
-    override def compareTo(that: SingleStreamIterator[A, B]): Int = this.nextEntry.key.compareTo(that.nextEntry.key)
+    /** The way the iterators are used it is guaranteed that we'll never compare an empty iterator. */
+    override def compareTo(that: SortedIterator): Int = this.nextKey.compareTo(that.nextKey)
 
+    /** Closes the underlying stream. */
     def close(): Unit = this.stream.safelyClose()
   }
 
-  /** An iterator that merges sorted streams on the fly. */
-  private class MergingIterator[A,B <: Comparable[B]](val streams: Seq[InputStream],
-                                                      codec: SortingCollection.Codec[A],
-                                                      val keyfunc: A => B) extends Iterator[A] {
-    private val iterators = new util.TreeSet[SingleStreamIterator[A,B]]()
+
+  /**
+    * An iterator that merges records from [[SortedIterator]]s and maintains ordering.
+    */
+  private class MergingIterator(val streams: Seq[InputStream]) extends Iterator[A] with Closeable {
+    private val iterators = new util.PriorityQueue[SortedIterator]()
     streams.foreach { stream =>
-      val iter = new SingleStreamIterator[A,B](stream, copyOf(codec), keyfunc)
+      val iter = new SortedIterator(stream, copyOf(codec))
       if (iter.hasNext) iterators.add(iter)
     }
 
-    /* This method exists because the scala compiler doesn't like the public clone() override in codec. */
+    /**
+      * This method exists because the scala compiler doesn't like the public clone() override in codec.
+      * It's ugly, but it works.
+      */
     private def copyOf(codec: SortingCollection.Codec[A]):  SortingCollection.Codec[A] = {
       codec.getClass.getMethod("clone").invoke(codec).asInstanceOf[SortingCollection.Codec[A]]
     }
@@ -151,28 +187,26 @@ object Sorter {
 
     override def next(): A = {
       if (!hasNext) throw new NoSuchElementException("next called on empty iterator.")
-      val iter = this.iterators.pollFirst()
+      val iter = this.iterators.poll()
       val entry = iter.next()
       if (iter.hasNext) this.iterators.add(iter)
       else iter.close()
 
-      entry.rec
+      entry
+    }
+
+    /** Closes all underlying iterators/streams and frees the iterators. */
+    override def close(): Unit = {
+      this.iterators.foreach(i => i.safelyClose())
+      this.iterators.clear()
     }
   }
-}
 
-/** An implementation of a disk-backed sorting system. */
-class Sorter[A,B <: Comparable[B]](val maxRecordsInRam: Int,
-                                   private val codec: SortingCollection.Codec[A],
-                                   private val keyfunc: A => B) extends Iterable[A] {
-  private val stash = new Array[SortEntry[B]](maxRecordsInRam)
-  private val files = new ArrayBuffer[Path]()
-  private var recordsInMemory: Int = 0
-  private val byteStream = new ByteArrayOutputStream(128 * 1024) // TODO: how to size this?
-  private val _tmpStreamFactory = new TempStreamFactory
 
-  codec.setOutputStream(byteStream)
-
+  /**
+    * Adds a record to the sorter.  This is an amortized constant time operation, but the individual times
+    * will vary wildly as the accrued objects are written to disk once the max in memory is reached.
+    */
   def +=(rec: A): Unit = {
     val key = keyfunc(rec)
     this.codec.encode(rec)
@@ -184,6 +218,10 @@ class Sorter[A,B <: Comparable[B]](val maxRecordsInRam: Int,
     if (recordsInMemory == maxRecordsInRam) spill()
   }
 
+  /**
+    * Writes out a temporary file containing all the accrued objects and releases the objects so
+    * that they can be garbage collected.
+    */
   private def spill(): Unit = {
     if (recordsInMemory > 0) {
       util.Arrays.parallelSort(stash, 0, recordsInMemory)
@@ -200,12 +238,19 @@ class Sorter[A,B <: Comparable[B]](val maxRecordsInRam: Int,
     }
   }
 
+  /**
+    * Generates an iterator over the data accumulated so far.  May be called multiple times to iterate
+    * over the data multiple times, and may even be invoked between adding more objects. Note however
+    * that each call to iterator will cause any accumulated objects to be written to disk, so it should
+    * not be invoked too frequently!
+     */
   def iterator: Iterator[A] = {
     spill()
     val streams = files.map(f => _tmpStreamFactory.wrapTempInputStream(Io.toInputStream(f), Io.bufferSize))
-    new MergingIterator(streams, codec, keyfunc)
+    new MergingIterator(streams)
   }
 
+  /** Deletes any temporary files that have been created. */
   def close(): Unit = {
     this.files.foreach(Files.deleteIfExists)
   }
