23e5490a50fc23a66689a24bd46fe66b2eb839f9
Nils Homer <nilshomer@gmail.com>
2018-03-01 22:35:23 -0700

    Adding documentation for SampleBarcodeMetrics.

--- src/main/scala/com/fulcrumgenomics/util/SampleBarcodeMetric.scala
+++ src/main/scala/com/fulcrumgenomics/util/SampleBarcodeMetric.scala
@@ -94,7 +94,42 @@ object SampleBarcodeMetric {
 }
 
 /**
-  * Metrics for matching reads to sample barcodes.
+  * Metrics for matching reads to sample barcodes primarily used in [[com.fulcrumgenomics.fastq.DemuxFastqs]].
+  *
+  * NB: the counts of reads assumes that each read corresponds to a single cluster on an Illumina flowcell.  For example,
+  * the number of reads for a single-end sequencing run will equal the number of records in the output FASTQ/BAM file,
+  * while for a paired-end sequencing run this will match half the number of records in the output FASTQ/BAM file.
+  *
+  * @param barcode_name the name for the sample barcode, typically the sample name from the SampleSheet.
+  * @param library_name the name of the library, typically the library identifier from the SampleSheet.
+  * @param barcode the sample barcode bases.  Dual index barcodes will have two sample barcode sequences delimited by a
+  *                dash.
+  * @param reads the total number of reads matching the given barcode.
+  * @param pf_reads the total number of pass-filter reads matching the given barcode.
+  * @param perfect_matches the number of reads that match perfectly the given barcode.
+  * @param pf_perfect_matches the number of pass-filter reads that match perfectly the given barcode.
+  * @param one_mismatch_matches the number of pass-filter reads that match the given barcode with exactly one mismatch.
+  * @param pf_one_mismatch_matches the number of pass-filter reads that match the given barcode with exactly one mismatch.
+  * @param pct_matches the fraction of all reads that match the given barcode.
+  * @param ratio_this_barcode_to_best_barcode_pct the rate of all reads matching this barcode to all reads matching the
+  *                                                most prevalent barcode. For the most prevalent barcode this will be 1,
+  *                                                for all others it will be less than 1 (except for the possible
+  *                                                exception of when there are more unmatched reads than for any other
+  *                                                barcode, in which case the value may be arbitrarily large).  One over
+  *                                                the lowest number in this column gives you the fold-difference in
+  *                                                representation between barcodes.
+  * @param pf_pct_matches the fraction of all pass-filter reads that match the given barcode.
+  * @param pf_ratio_this_barcode_to_best_barcode_pct the rate of all pass-filter reads matching this barcode to all
+  *                                                  reads matching the most prevalent barcode. For the most prevalent
+  *                                                  barcode this will be 1, for all others it will be less than 1
+  *                                                  (except for the possible exception of when there are more unmatched
+  *                                                  reads than for any other  barcode, in which case the value may be
+  *                                                  arbitrarily large).  One over the lowest number in this column
+  *                                                  gives you the fold-difference in  representation between barcodes.
+  * @param pf_normalized_matches The "normalized" matches to each barcode. This is calculated as the number of
+  *                              pass-filter reads matching this barcode over the sum of all pass-filter reads matching
+  *                              any barcode (excluding unmatched). If all barcodes are represented equally this will be
+  *                              1.
   */
 case class SampleBarcodeMetric(
   var barcode_name: String                                         = "",
