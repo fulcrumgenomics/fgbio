a3f96799e7551409631ce91e6748ba77761b7c37
Nils Homer <nilshomer@gmail.com>
2020-05-10 23:13:57 -0700

    fixups and unit tests

--- src/main/scala/com/fulcrumgenomics/fasta/SequenceDictionary.scala
+++ src/main/scala/com/fulcrumgenomics/fasta/SequenceDictionary.scala
@@ -30,37 +30,51 @@ import com.fulcrumgenomics.FgBioDef._
 import enumeratum.EnumEntry
 import htsjdk.samtools.{SAMSequenceDictionary, SAMSequenceRecord}
 import scala.collection.mutable
+import SequenceMetadata.Keys
 
+object SequenceMetadata {
+  /** Keys for standard attributes in [[SequenceMetadata]] */
+  object Keys {
+    val Aliases              : String = "AN"
+    val AlternateLocus       : String = "AH"
+    val Assembly             : String = SAMSequenceRecord.ASSEMBLY_TAG
+    val Description          : String = SAMSequenceRecord.DESCRIPTION_TAG
+    private[fasta] val Length: String = SAMSequenceRecord.SEQUENCE_LENGTH_TAG
+    val Md5                  : String = SAMSequenceRecord.MD5_TAG
+    private[fasta] val Name  : String = SAMSequenceRecord.SEQUENCE_NAME_TAG
+    val Species              : String = SAMSequenceRecord.SPECIES_TAG
+    val Topology             : String = "TP"
+    val Uri                  : String = SAMSequenceRecord.URI_TAG
+  }
+}
 
 /** Stores information about a single Sequence (ex. chromosome, contig)
   *
   * @param name the primary name of the sequence
   * @param length the length of the sequence, or zero if unknown
+  * @param index the index in the sequence dictionary
   * @param attributes attributes of this sequence
   */
 case class SequenceMetadata(name: String,
                             length: Int,
+                            index: Int = 0,
                             attributes: Map[String, String] = Map.empty) {
   allNames.foreach { name => SAMSequenceRecord.validateSequenceName(name) }
   require(length >= 0, s"Length must be >= 0 for '$name'")
-  require(attributes.keys.forall(_ != SAMSequenceRecord.SEQUENCE_NAME_TAG),
-    f"`${SAMSequenceRecord.SEQUENCE_NAME_TAG}` should not given in the list of attributes")
-  require(attributes.keys.forall(_ != SAMSequenceRecord.SEQUENCE_LENGTH_TAG),
-    s"`${SAMSequenceRecord.SEQUENCE_LENGTH_TAG}` should not given in the list of attributes")
+  require(attributes.keys.forall(_ != Keys.Name),
+    f"`${Keys.Name}` should not given in the list of attributes")
+  require(attributes.keys.forall(_ != Keys.Length),
+    s"`${Keys.Length}` should not given in the list of attributes")
 
   @inline final def apply(key: String): String = this.attributes(key)
-  @inline final def string(key: String): String = this.attributes(key).toString
   @inline final def get(key: String): Option[String] = this.attributes.get(key)
-  @inline final def getString(key: String): Option[String] = this.attributes.get(key).map(_.toString)
   @inline final def contains(key: String): Boolean = this.attributes.contains(key)
-
-  @inline final def aliases: Seq[String] = this.get("AN").getOrElse("").split(',')
+  @inline final def aliases: Seq[String] = this.get(Keys.Aliases).map(_.split(',').toSeq).getOrElse(Seq.empty[String])
   /** All names, including aliases */
   @inline final def allNames: Seq[String] = name +: aliases
-
   @inline final def isAlternate: Boolean = this.alternate.isDefined
   lazy val alternate: Option[AlternateLocus] = {
-    this.get("AH").flatMap {
+    this.get(Keys.AlternateLocus).flatMap {
       case "*"   => None
       case range =>
         val (refName: String, start: Int, end: Int) = FgBioDef.parseRange(range)
@@ -71,21 +85,19 @@ case class SequenceMetadata(name: String,
         Some(locus)
     }
   }
-
-  @inline final def md5: Option[String] = this.get(SAMSequenceRecord.MD5_TAG)
+  @inline final def md5: Option[String] = this.get(Keys.Md5)
   @inline final def md5Int: Option[BigInt] = this.md5.map(BigInt(_, 16))
-
-  @inline final def assembly: Option[String] = this.get(SAMSequenceRecord.ASSEMBLY_TAG)
-  @inline final def uri: Option[String] = this.get(SAMSequenceRecord.URI_TAG)
-  @inline final def species: Option[String] = this.get(SAMSequenceRecord.SPECIES_TAG)
-  //@inline final def description: Option[String] = this.get(SAMSequenceRecord.DESCRIPTION_TAG) // FIXME
+  @inline final def assembly: Option[String] = this.get(Keys.Assembly)
+  @inline final def uri: Option[String] = this.get(Keys.Uri)
+  @inline final def species: Option[String] = this.get(Keys.Species)
+  @inline final def description: Option[String] = this.get(Keys.Description)
   @inline final def topology: Option[Topology] = {
-    this.get("TP").flatMap(tp => Topology.values.find(_.name == tp))
+    this.get(Keys.Topology).flatMap(tp => Topology.values.find(_.name == tp))
   }
 
   /** Returns true if the the sequences share a common reference name (including aliases), have the same length, and
     * the same MD5 if both have MD5s. */
-  def same(that: SequenceMetadata): Boolean = {
+  def sameAs(that: SequenceMetadata): Boolean = {
     val md5Match = (this.md5Int, that.md5Int) match {
       case (Some(thisMd5Int), Some(thatMd5Int)) => thisMd5Int == thatMd5Int
       case _                                    => true
@@ -95,7 +107,16 @@ case class SequenceMetadata(name: String,
       (this.name == that.name || this.allNames.exists(that.allNames.contains)) &&
       md5Match
   }
+}
 
+object SequenceDictionary {
+  /** Builds a sequence dictionary from one or more sequence metadatas.  This will set the sequence index. */
+  def apply(info: SequenceMetadata*): SequenceDictionary = {
+    val infos = info.zipWithIndex.map { case (info, index) =>
+      if (info.index != index) info.copy(index=index) else info
+    }.toIndexedSeq
+    SequenceDictionary(infos=infos)
+  }
 }
 
 /** Contains an ordered collection of sequences. */
@@ -104,7 +125,8 @@ case class SequenceDictionary(infos: IndexedSeq[SequenceMetadata]) extends Itera
   private val mapping: Map[String, SequenceMetadata] = {
     // validates that the same name is not present twice across allNames (so includes aliases)
     val names: mutable.Set[String] = mutable.HashSet[String]()
-    infos.flatMap { info =>
+    infos.iterator.zipWithIndex.flatMap { case (info, index) =>
+      require(info.index == index, s"Infos must be given with index set correctly. See ${index}th with name: $names")
       info.allNames.map { name =>
         require(!names.contains(name), f"Found duplicate sequence name: $name")
         names.add(name)
@@ -118,51 +140,57 @@ case class SequenceDictionary(infos: IndexedSeq[SequenceMetadata]) extends Itera
   def contains(name: String): Boolean = this.mapping.contains(name)
   def apply(index: Int): SequenceMetadata = this.infos(index)
   override def iterator: Iterator[SequenceMetadata] = this.infos.iterator
+  def length: Int = this.infos.length
 }
 
 
 /** Contains useful converters to and from HTSJDK objects. */
 object Converters {
 
-  implicit class ToSequenceRecord(info: SequenceMetadata) {
-    def asSam(index: Option[Int] = None): SAMSequenceRecord = {
+  /** Converter from a [[SequenceMetadata]] to a [[SAMSequenceRecord]] */
+  implicit class ToSAMSequenceRecord(info: SequenceMetadata) {
+    def asSam: SAMSequenceRecord = {
       val rec = new SAMSequenceRecord(info.name, info.length)
-      index.foreach(rec.setSequenceIndex)
+      rec.setSequenceIndex(info.index)
       info.attributes.foreach { case (key, value) => rec.setAttribute(key, value.toString) }
       rec
     }
   }
 
-  implicit class FromSequenceRecord(rec: SAMSequenceRecord) {
-    def fromSam(): SequenceMetadata = {
+  /** Converter from a [[SAMSequenceRecord]] to a [[SequenceMetadata]] */
+  implicit class FromSAMSequenceRecord(rec: SAMSequenceRecord) {
+    def fromSam: SequenceMetadata = {
       val attributes: Map[String, String] = rec.getAttributes.map { entry =>
         entry.getKey -> entry.getValue
       }.toMap
       SequenceMetadata(
         name       = rec.getSequenceName,
         length     = rec.getSequenceLength,
+        index      = rec.getSequenceIndex,
         attributes = attributes
       )
     }
   }
 
-  implicit class ToSequenceDictionary(infos: SequenceDictionary) {
-    def asSam(): SAMSequenceDictionary = {
+  /** Converter from a [[SequenceDictionary]] to a [[SAMSequenceDictionary]] */
+  implicit class ToSAMSequenceDictionary(infos: SequenceDictionary) {
+    def asSam: SAMSequenceDictionary = {
       val recs = infos.iterator.zipWithIndex.map { case (info, index) =>
-        info.asSam(index=Some(index))
+        info.copy(index=index).asSam
       }.toJavaList
       new SAMSequenceDictionary(recs)
     }
   }
 
-  implicit class FromSequenceDictionary(dict: SAMSequenceDictionary) {
-    def fromSam(): SequenceDictionary = SequenceDictionary(dict.getSequences.map(_.fromSam()).toIndexedSeq)
+  /** Converter from a [[SAMSequenceDictionary]] to a [[SequenceDictionary]] */
+  implicit class FromSAMSequenceDictionary(dict: SAMSequenceDictionary) {
+    def fromSam: SequenceDictionary = SequenceDictionary(dict.getSequences.map(_.fromSam).toIndexedSeq)
   }
 }
 
 /** The base trait for all topologies. */
 sealed trait Topology extends EnumEntry {
-  def name: String = this.name.toLowerCase
+  def name: String = this.entryName.toLowerCase
 }
 
 /** An enumeration representing the various reference sequence topologies. */
--- src/test/scala/com/fulcrumgenomics/fasta/SequenceDictionaryTest.scala
+++ src/test/scala/com/fulcrumgenomics/fasta/SequenceDictionaryTest.scala
@@ -25,10 +25,31 @@
 
 package com.fulcrumgenomics.fasta
 
+import com.fulcrumgenomics.fasta.Converters.{FromSAMSequenceDictionary, FromSAMSequenceRecord, ToSAMSequenceDictionary, ToSAMSequenceRecord}
 import com.fulcrumgenomics.testing.UnitSpec
 import org.scalatest.OptionValues
+import com.fulcrumgenomics.fasta.SequenceMetadata.Keys
+import com.fulcrumgenomics.fasta.Topology.{Circular, Linear}
+import htsjdk.samtools.{SAMSequenceDictionary, SAMSequenceRecord}
+import com.fulcrumgenomics.FgBioDef._
 
 class SequenceDictionaryTest extends UnitSpec with OptionValues {
+
+  private val validSequence = SequenceMetadata(
+    name = "chr1",
+    length = 100,
+    attributes = Map(
+      "key1"           -> "value1",
+      "key2"           -> "value2",
+      Keys.Aliases     -> "chr1_1,chr1_2,chr1_3",
+      Keys.Assembly    -> "assembly",
+      Keys.Description -> "description",
+      Keys.Md5         -> "123",
+      Keys.Species     -> "species",
+    )
+  )
+
+  private val emptySequence  = SequenceMetadata(name="chr1", length=123)
 
   "SequenceMetadata" should "fail if not a validated sequence" in {
     an[Exception] should be thrownBy SequenceMetadata(name="(((242*(@&$)))", length=12)
@@ -47,41 +68,28 @@ class SequenceDictionaryTest extends UnitSpec with OptionValues {
   }
 
   it should "implement apply, get, and contains" in {
-    val sequence = SequenceMetadata(
-      name = "chr1",
-      length = 100,
-      attributes = Map(
-        "key1" -> "value1",
-        "key2" -> "value2",
-      )
-    )
-
     // apply
-    sequence("key1") shouldBe "value1"
-    an[Exception] should be thrownBy sequence("key3")
+    validSequence("key1") shouldBe "value1"
+    an[Exception] should be thrownBy validSequence("key3")
 
     // get
-    sequence.get("key1").value shouldBe "value1"
-    sequence.get("key3") shouldBe 'empty
+    validSequence.get("key1").value shouldBe "value1"
+    validSequence.get("key3") shouldBe 'empty
 
     // contains
-    sequence.contains("key1") shouldBe true
-    sequence.contains("key3") shouldBe false
+    validSequence.contains("key1") shouldBe true
+    validSequence.contains("key3") shouldBe false
   }
 
-  it should "return all aliases if present in AN" in {
-    val sequence = SequenceMetadata(
-      name = "chr1",
-      length = 100,
-      attributes = Map(
-        "AN" -> "chr1_1,chr1_2,chr1_3"
-      )
-    )
-    sequence.allNames should contain theSameElementsInOrderAs Seq("chr1", "chr1_1", "chr1_2", "chr1_3")
-    sequence.aliases should contain theSameElementsInOrderAs Seq("chr1_1", "chr1_2", "chr1_3")
+  "SequenceMetadata.allNames" should "return all names including aliases" in {
+    validSequence.allNames should contain theSameElementsInOrderAs Seq("chr1", "chr1_1", "chr1_2", "chr1_3")
   }
 
-  it should "return if the sequence is an alternate locus" in {
+  "SequenceMetadata.aliases" should "return all names including aliases" in {
+    validSequence.aliases should contain theSameElementsInOrderAs Seq("chr1_1", "chr1_2", "chr1_3")
+  }
+
+  "SequenceMetadata.isAlternate" should "return if the sequence is an alternate locus" in {
     val noAltLocus  = SequenceMetadata(name="chr1", length=123)
     val altStar     = SequenceMetadata(name="chr1", length=123, attributes=Map("AH" -> "*"))
     val locusEquals = SequenceMetadata(name="chr1", length=123, attributes=Map("AH" -> "=:1-2"))
@@ -100,19 +108,154 @@ class SequenceDictionaryTest extends UnitSpec with OptionValues {
     locusFull.alternate.value shouldBe AlternateLocus(refName="chr4", start=2, end=3)
   }
 
-  // md5
+  "SequenceMetadata.md5" should "return the md5 checksum if present" in {
+    validSequence.md5.value shouldBe "123"
+    validSequence.md5Int.value shouldBe BigInt("123", 16)
+    emptySequence.md5 shouldBe 'empty
+    emptySequence.md5Int shouldBe 'empty
+  }
 
-  // assembly
+  "SequenceMetadata.assembly" should "return the assembly if present" in {
+    validSequence.assembly.value shouldBe "assembly"
+    emptySequence.assembly shouldBe 'empty
+  }
 
-  // species
+  "SequenceMetadata.species" should "return the species if present" in {
+    validSequence.species.value shouldBe "species"
+    emptySequence.species shouldBe 'empty
+  }
 
-  // description
+  "SequenceMetadata.description" should "return the description if present" in {
+    validSequence.description.value shouldBe "description"
+    emptySequence.description shouldBe 'empty
+  }
 
-  // topology
+  "SequenceMetadata.topology" should "return the topology if present" in {
+    val linear   = SequenceMetadata(name="chr1", length=123, attributes=Map(Keys.Topology -> Topology.Linear.name))
+    val circular = SequenceMetadata(name="chr1", length=123, attributes=Map(Keys.Topology -> Topology.Circular.name))
 
-  // same
+    linear.topology.value shouldBe Linear
+    circular.topology.value shouldBe Circular
+    emptySequence.topology shouldBe 'empty
+  }
 
-  // SequenceDictionary
+  "SequenceMetadata.sameAs" should "return false if not reference names are shared" in {
+    SequenceMetadata(name="chr1", length=0) sameAs SequenceMetadata(name="chr2", length=0) shouldBe false
+    SequenceMetadata(name="chr1", length=0) sameAs SequenceMetadata(name="chr2", length=0, attributes=Map(Keys.Aliases -> "chr3")) shouldBe false
+    SequenceMetadata(name="chr1", length=0) sameAs SequenceMetadata(name="chr2", length=0, attributes=Map(Keys.Aliases -> "chr3,chr4")) shouldBe false
+  }
 
-  // Converters
+  it should "return false if the lengths are different" in {
+    SequenceMetadata(name="chr1", length=0) sameAs SequenceMetadata(name="chr1", length=1) shouldBe false
+  }
+
+
+  it should "return false if the md5s are different" in {
+    SequenceMetadata(name="chr1", length=0, attributes=Map(Keys.Md5 -> "1")) sameAs SequenceMetadata(name="chr1", length=0, attributes=Map(Keys.Md5 -> "2")) shouldBe false
+  }
+
+  it should "return true if the primary name is the same" in {
+    SequenceMetadata(name="chr1", length=0) sameAs SequenceMetadata(name="chr1", length=0) shouldBe true
+  }
+
+  it should "return true if the primary name in one matches an alias in the other" in {
+    SequenceMetadata(name="chr1", length=0) sameAs SequenceMetadata(name="chr2", length=0, attributes=Map(Keys.Aliases -> "chr1")) shouldBe true
+    SequenceMetadata(name="chr2", length=0, attributes=Map(Keys.Aliases -> "chr1")) sameAs SequenceMetadata(name="chr1", length=0) shouldBe true
+  }
+
+  it should "return true if they share an alias" in {
+    SequenceMetadata(name="chrX", length=0, attributes=Map(Keys.Aliases -> "chr1")) sameAs SequenceMetadata(name="chrY", length=0, attributes=Map(Keys.Aliases -> "chr1")) shouldBe true
+    SequenceMetadata(name="chrX", length=0, attributes=Map(Keys.Aliases -> "chr1,chr2")) sameAs SequenceMetadata(name="chrY", length=0, attributes=Map(Keys.Aliases -> "chr2,chr3")) shouldBe true
+  }
+
+  it should "return true if the name, length, and md5 match" in {
+    SequenceMetadata(name="chr1", length=0, attributes=Map(Keys.Md5 -> "1")) sameAs SequenceMetadata(name="chr1", length=0, attributes=Map(Keys.Md5 -> "1")) shouldBe true
+  }
+
+  "SequenceDictionary" should "fail to build two sequence metadatas share a name (including aliases)" in {
+    val one   = SequenceMetadata(name="chr1", length=0)
+    val two   = SequenceMetadata(name="chr2", length=0, attributes=Map(Keys.Aliases -> "chr1"))
+    val three = SequenceMetadata(name="chr3", length=0, attributes=Map(Keys.Aliases -> "chr0,chr1,chrX"))
+
+    an[Exception] should be thrownBy SequenceDictionary(one, one)
+    an[Exception] should be thrownBy SequenceDictionary(one, two)
+    an[Exception] should be thrownBy SequenceDictionary(two, three)
+  }
+
+  it should "implement apply, get, contains, and iterator" in {
+    val one   = SequenceMetadata(name="1", length=0)
+    val two   = SequenceMetadata(name="2", length=0, attributes=Map(Keys.Aliases -> "1_alt"))
+    val three = SequenceMetadata(name="3", length=0, attributes=Map(Keys.Aliases -> "3_alt_1,3_alt_2,3_alt_3"))
+    val dict  = SequenceDictionary(one, two, three)
+
+    dict.infos.length shouldBe 3
+    dict.infos.zip(Seq(one, two, three)).foreach { case (left, right) => left sameAs right shouldBe true }
+    dict.iterator.zip(Iterator(one, two, three)).foreach { case (left, right) => left sameAs right shouldBe true }
+    dict.zip(Seq(one, two, three)).foreach { case (left, right) => left sameAs right shouldBe true }
+
+
+    dict.infos.zipWithIndex.foreach { case (seq, index) =>
+      seq.index shouldBe index
+      dict(index) shouldBe seq
+      seq.allNames.foreach { name =>
+        dict(name) shouldBe seq
+        dict.get(name).value shouldBe seq
+        dict.contains(name) shouldBe true
+      }
+    }
+
+    an[IndexOutOfBoundsException] should be thrownBy dict.apply(3)
+    an[Exception] should be thrownBy dict("4")
+    dict.get("4") shouldBe 'empty
+    dict.contains("4") shouldBe false
+  }
+
+  "Converters.ToSequenceRecord" should "convert from a [[SequenceMetadata]] to a [[SAMSequenceRecord]]" in {
+    val record: SAMSequenceRecord = new ToSAMSequenceRecord(validSequence).asSam
+    record.getSequenceName shouldBe validSequence.name
+    record.getSequenceLength shouldBe validSequence.length
+    record.getSequenceIndex shouldBe validSequence.index
+    record.getAssembly shouldBe validSequence.assembly.value
+    record.getDescription shouldBe validSequence.description.value
+    record.getMd5 shouldBe validSequence.md5.value
+    record.getSpecies shouldBe validSequence.species.value
+
+    validSequence.attributes.foreach { case (key, value) => record.getAttribute(key) shouldBe value }
+  }
+
+  "Converters.FromSequenceRecord" should "convert from a [[SAMSequenceRecord]] to a [[SequenceMetadata]]" in {
+    // Note: relies on the above test that converts from a [[SequenceMetadata]] to a [[SAMSequenceRecord]]
+    val record: SequenceMetadata = new FromSAMSequenceRecord(new ToSAMSequenceRecord(validSequence).asSam).fromSam
+    record shouldBe validSequence
+    validSequence.attributes.foreach { case (key, value) => record(key) shouldBe value }
+  }
+
+  "Converters.ToSequenceDictionary" should "convert from a [[SequenceDictionary]] to a [[SAMSequenceDictionary]]" in {
+    val scalaDict: SequenceDictionary = SequenceDictionary(
+      validSequence,
+      SequenceMetadata(name="chrX", length=234),
+      SequenceMetadata(name="chrY", length=345)
+    )
+    val javaDict: SAMSequenceDictionary = new ToSAMSequenceDictionary(scalaDict).asSam
+
+    javaDict.getReferenceLength shouldBe scalaDict.infos.map(_.length).sum
+    javaDict.getSequences.length shouldBe scalaDict.length
+    javaDict.getSequences.iterator().zip(scalaDict.iterator).foreach {
+      case (javaRecord: SAMSequenceRecord, scalaRecord: SequenceMetadata) =>
+        // Note: relies on the above test that converts from a [[SequenceMetadata]] to a [[SAMSequenceRecord]]
+        new FromSAMSequenceRecord(javaRecord).fromSam shouldBe scalaRecord
+    }
+  }
+
+  "Converters.FromSAMSequenceDictionary" should "convert from a [[SAMSequenceDictionary]] to a [[SequenceDictionary]]" in {
+    // Note: relies on the above tests that converts from a [[SequenceDictionary]] to a [[SAMSequenceDictionary]]
+    val sourceDict: SequenceDictionary = SequenceDictionary(
+      validSequence,
+      SequenceMetadata(name="chrX", length=234),
+      SequenceMetadata(name="chrY", length=345)
+    )
+    val targetDict: SequenceDictionary = new FromSAMSequenceDictionary(new ToSAMSequenceDictionary(sourceDict).asSam).fromSam
+
+    sourceDict shouldBe targetDict
+  }
 }
