8fb675476319e285020a774414166e4b3c548291
Clint Valentine <valentine.clint@gmail.com>
2021-08-28 14:38:30 -0400

    Implement a few tests for Variant as a locatable

--- src/test/scala/com/fulcrumgenomics/vcf/api/VariantTest.scala
+++ src/test/scala/com/fulcrumgenomics/vcf/api/VariantTest.scala
@@ -24,11 +24,15 @@
 
 package com.fulcrumgenomics.vcf.api
 
-import com.fulcrumgenomics.FgBioDef._
 import com.fulcrumgenomics.testing.UnitSpec
+import htsjdk.samtools.util.Locatable
 import org.scalatest.OptionValues
+import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+
+import scala.collection.immutable.ListMap
 
 class VariantTest extends UnitSpec with OptionValues {
+
   "Variant.isMissingValue" should "correctly handle missing and non-missing values" in {
     Variant.isMissingValue(".") shouldBe true
     Variant.isMissingValue('.') shouldBe true
@@ -38,5 +42,21 @@ class VariantTest extends UnitSpec with OptionValues {
     Variant.isMissingValue(".1")      shouldBe false
     Variant.isMissingValue(Float.NaN) shouldBe false
     Range(-500, 500).foreach { i => Variant.isMissingValue(i) shouldBe false}
+  }
+
+  "Variant" should "be locatable when it contains a simple allele" in {
+    val variant = Variant("chr1", 10, alleles = AlleleSet(Allele("A")))
+    variant mustBe a[Locatable]
+    variant.contig shouldBe "chr1"
+    variant.start shouldBe 10
+    variant.end shouldBe 10
+  }
+
+  it should "be locatable when it contains a symbolic allele that is a deletion" in {
+    val variant = Variant("chr1", 10, alleles = AlleleSet(Allele("A"), Allele("<DEL>")), attrs = ListMap("END" -> 200))
+    variant mustBe a[Locatable]
+    variant.contig shouldBe "chr1"
+    variant.start shouldBe 10
+    variant.end shouldBe 200
   }
 }
