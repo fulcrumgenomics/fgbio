61858f1c30f15d64098c345ac0f5428a0f9fd1b1
Nathan Roach <nathan@fulcrumgenomics.com>
2022-02-10 14:22:14 -0700

    Fixing test after addressing nils PR comments

--- src/test/scala/com/fulcrumgenomics/fasta/SortSequenceDictionaryTest.scala
+++ src/test/scala/com/fulcrumgenomics/fasta/SortSequenceDictionaryTest.scala
@@ -47,27 +47,27 @@ class SortSequenceDictionaryTest extends UnitSpec {
   )
 
   private val chr1Alias = SequenceMetadata(
-    name="chr1_1",
-    length=100,
+    name   = "chr1_1",
+    length = 100,
   )
   private val chr2Alias = SequenceMetadata(
-    name="chr2_1",
-    length=100,
+    name   = "chr2_1",
+    length = 100,
   )
   private val chrMAlias = SequenceMetadata(
-    name="chrM_1",
-    length=100,
+    name   = "chrM_1",
+    length = 100,
   )
 
-  private val baseDictionary = SequenceDictionary(chr1, chr2, chrM)
+  private val dictChr1Chr2ChrM = SequenceDictionary(chr1, chr2, chrM)
 
-  "SortSequenceDictionary" should "Keep output identical to input if the sort order is the same" in {
+  "SortSequenceDictionary" should "keep output identical to input if the sort order is the same" in {
     val inputDictionaryPath  = makeTempFile("test.", ".1.dict")
-    val sortDictionaryPath = makeTempFile("test.", ".2.dict")
+    val sortDictionaryPath   = makeTempFile("test.", ".2.dict")
     val outputDictionaryPath = makeTempFile("test.", ".3.dict")
 
-    baseDictionary.write(inputDictionaryPath)
-    baseDictionary.write(sortDictionaryPath)
+    dictChr1Chr2ChrM.write(inputDictionaryPath)
+    dictChr1Chr2ChrM.write(sortDictionaryPath)
 
     val tool = new SortSequenceDictionary(
       input         = inputDictionaryPath,
@@ -76,153 +76,141 @@ class SortSequenceDictionaryTest extends UnitSpec {
     )
     executeFgbioTool(tool)
 
-    val outputDictionary = SequenceDictionary(outputDictionaryPath)
-
-    outputDictionary.sameAs(baseDictionary) shouldBe true
-
+    SequenceDictionary(outputDictionaryPath).sameAs(dictChr1Chr2ChrM) shouldBe true
   }
   
-  it should "Append metadata row if contig is present in input and not sortDictionary, and appendMissingContigs is True" in {
+  it should "append metadata row if contig is present in input and not sortDictionary, and `skip-missing-contigs` is false" in {
     val inputDictionaryPath  = makeTempFile("test.", ".1.dict")
-    val sortDictionaryPath = makeTempFile("test.", ".2.dict")
+    val sortDictionaryPath   = makeTempFile("test.", ".2.dict")
     val outputDictionaryPath = makeTempFile("test.", ".3.dict")
 
-    val subsetDictionary = SequenceDictionary(chr2, chrM)
+    val subsetDictionary   = SequenceDictionary(chr2, chrM)
     val expectedDictionary = SequenceDictionary(chr2, chrM, chr1)
 
-    baseDictionary.write(inputDictionaryPath)
+    dictChr1Chr2ChrM.write(inputDictionaryPath)
     subsetDictionary.write(sortDictionaryPath)
 
     val tool = new SortSequenceDictionary(
-      input         = inputDictionaryPath,
-      sortDictionary = sortDictionaryPath,
-      output        = outputDictionaryPath,
+      input              = inputDictionaryPath,
+      sortDictionary     = sortDictionaryPath,
+      output             = outputDictionaryPath,
+      skipMissingContigs = false,
     )
     executeFgbioTool(tool)
 
-    val outputDictionary = SequenceDictionary(outputDictionaryPath)
-
-    outputDictionary.sameAs(expectedDictionary) shouldBe true
+    SequenceDictionary(outputDictionaryPath).sameAs(expectedDictionary) shouldBe true
   }
 
-  it should "Skip metadata row if contig is present in input and not sortDictionary, and appendMissingContigs is False" in {
+  it should "skip metadata row if contig is present in input and not sortDictionary, and `--skip-missing-contigs` is true" in {
     val inputDictionaryPath  = makeTempFile("test.", ".1.dict")
-    val sortDictionaryPath = makeTempFile("test.", ".2.dict")
+    val sortDictionaryPath   = makeTempFile("test.", ".2.dict")
     val outputDictionaryPath = makeTempFile("test.", ".3.dict")
 
     val subsetDictionary = SequenceDictionary(chr2, chrM)
 
-    baseDictionary.write(inputDictionaryPath)
+    dictChr1Chr2ChrM.write(inputDictionaryPath)
     subsetDictionary.write(sortDictionaryPath)
 
     val tool = new SortSequenceDictionary(
-      input         = inputDictionaryPath,
-      sortDictionary = sortDictionaryPath,
-      output        = outputDictionaryPath,
-      appendMissingContigs = false,
+      input              = inputDictionaryPath,
+      sortDictionary     = sortDictionaryPath,
+      output             = outputDictionaryPath,
+      skipMissingContigs = true,
     )
     executeFgbioTool(tool)
 
-    val outputDictionary = SequenceDictionary(outputDictionaryPath)
-
-    outputDictionary.sameAs(subsetDictionary) shouldBe true
+    SequenceDictionary(outputDictionaryPath).sameAs(subsetDictionary) shouldBe true
   }
 
-  it should "Skip metadata row if contig is present in sortDictionary and not input" in {
+  it should "skip metadata row if contig is present in sortDictionary and not input" in {
     val inputDictionaryPath  = makeTempFile("test.", ".1.dict")
-    val sortDictionaryPath = makeTempFile("test.", ".2.dict")
+    val sortDictionaryPath   = makeTempFile("test.", ".2.dict")
     val outputDictionaryPath = makeTempFile("test.", ".3.dict")
 
     val subsetDictionary = SequenceDictionary(chr2, chrM)
 
     subsetDictionary.write(inputDictionaryPath)
-    baseDictionary.write(sortDictionaryPath)
+    dictChr1Chr2ChrM.write(sortDictionaryPath)
 
     val tool = new SortSequenceDictionary(
-      input         = inputDictionaryPath,
+      input          = inputDictionaryPath,
       sortDictionary = sortDictionaryPath,
-      output        = outputDictionaryPath,
+      output         = outputDictionaryPath,
     )
     executeFgbioTool(tool)
 
-    val outputDictionary = SequenceDictionary(outputDictionaryPath)
-
-    outputDictionary.sameAs(subsetDictionary) shouldBe true
+    SequenceDictionary(outputDictionaryPath).sameAs(subsetDictionary) shouldBe true
   }
 
-  it should "Reorder contigs if sortDictionary is provided in a different order" in {
+  it should "reorder contigs if sortDictionary is provided in a different order" in {
     val inputDictionaryPath  = makeTempFile("test.", ".1.dict")
-    val sortDictionaryPath = makeTempFile("test.", ".2.dict")
+    val sortDictionaryPath   = makeTempFile("test.", ".2.dict")
     val outputDictionaryPath = makeTempFile("test.", ".3.dict")
 
     val reorderedDictionary = SequenceDictionary(chrM, chr1, chr2)
 
-    baseDictionary.write(inputDictionaryPath)
+    dictChr1Chr2ChrM.write(inputDictionaryPath)
     reorderedDictionary.write(sortDictionaryPath)
 
     val tool = new SortSequenceDictionary(
-      input         = inputDictionaryPath,
+      input          = inputDictionaryPath,
       sortDictionary = sortDictionaryPath,
-      output        = outputDictionaryPath,
+      output         = outputDictionaryPath,
     )
     executeFgbioTool(tool)
 
-    val outputDictionary = SequenceDictionary(outputDictionaryPath)
-
-    outputDictionary.sameAs(reorderedDictionary) shouldBe true
+    SequenceDictionary(outputDictionaryPath).sameAs(reorderedDictionary) shouldBe true
   }
 
-  it should "Reorder contigs even if contigs in input are an alias of contigs in sort dictionary" in {
-    val aliasDictionary = SequenceDictionary(chr1Alias, chr2Alias, chrMAlias)
+  it should "reorder contigs even if contigs in input are an alias of contigs in sort dictionary" in {
+    val aliasDictionary      = SequenceDictionary(chr1Alias, chr2Alias, chrMAlias)
     val inputDictionaryPath  = makeTempFile("test.", ".1.dict")
-    val sortDictionaryPath = makeTempFile("test.", ".2.dict")
+    val sortDictionaryPath   = makeTempFile("test.", ".2.dict")
     val outputDictionaryPath = makeTempFile("test.", ".3.dict")
 
     val reorderedDictionary = SequenceDictionary(chrM, chr1, chr2)
-    val expectedDictionary = SequenceDictionary(chrMAlias, chr1Alias, chr2Alias)
+    val expectedDictionary  = SequenceDictionary(chrMAlias, chr1Alias, chr2Alias)
 
 
     aliasDictionary.write(inputDictionaryPath)
     reorderedDictionary.write(sortDictionaryPath)
 
     val tool = new SortSequenceDictionary(
-      input         = inputDictionaryPath,
+      input          = inputDictionaryPath,
       sortDictionary = sortDictionaryPath,
-      output        = outputDictionaryPath,
+      output         = outputDictionaryPath,
     )
     executeFgbioTool(tool)
 
-    val outputDictionary = SequenceDictionary(outputDictionaryPath)
-
-    outputDictionary.sameAs(expectedDictionary) shouldBe true
+    SequenceDictionary(outputDictionaryPath).sameAs(expectedDictionary) shouldBe true
   }
 
   it should "raise Error over duplicate entries that point to the same contig in the input" in {
     val inputDictionaryPath  = makeTempFile("test.", ".1.dict")
-    val sortDictionaryPath = makeTempFile("test.", ".2.dict")
+    val sortDictionaryPath   = makeTempFile("test.", ".2.dict")
     val outputDictionaryPath = makeTempFile("test.", ".3.dict")
 
   val chr1IncorrectAlias = SequenceMetadata(
-    name             = "chr1_2",
-    length           = 100,
-    aliases          = Seq("chr1_3"),
+    name    = "chr1_2",
+    length  = 100,
+    aliases = Seq("chr1_3"),
   )
 
     val chrMIncorrectAlias = SequenceMetadata(
-    name             = "MT",
-    length           = 100,
-    aliases          = Seq( "chr1_1"),
+    name    = "MT",
+    length  = 100,
+    aliases = Seq( "chr1_1"),
   )
 
     val duplicateAliasDictionary = SequenceDictionary(chr1IncorrectAlias, chr2Alias, chrMIncorrectAlias)
 
-    baseDictionary.write(inputDictionaryPath)
+    dictChr1Chr2ChrM.write(inputDictionaryPath)
     duplicateAliasDictionary.write(sortDictionaryPath)
 
     val tool = new SortSequenceDictionary(
-      input         = inputDictionaryPath,
+      input          = inputDictionaryPath,
       sortDictionary = sortDictionaryPath,
-      output        = outputDictionaryPath,
+      output         = outputDictionaryPath,
     )
     val ex = intercept[Exception] {executeFgbioTool(tool) }
     ex.getMessage should include ("Found duplicate sequence name:")
